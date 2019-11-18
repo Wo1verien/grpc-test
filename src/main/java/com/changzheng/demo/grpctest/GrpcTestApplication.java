48f2624d5e92e6496f13a2822f77e4568e91a758
changzheng <changzheng@mafengwo.com>
2019-11-18 17:35:48 +0800

    first commit

---
+++ src/main/java/com/changzheng/demo/grpctest/GrpcTestApplication.java
@@ -0,0 +1,13 @@
+package com.changzheng.demo.grpctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/changzheng/demo/grpctest/GrpcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changzheng.demo.grpctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
